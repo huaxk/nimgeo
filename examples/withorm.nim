c516d89a0df37f7c3ba672fbcd2df82e81a46f18
huaxk <huaxk@163.com>
2019-09-22 13:06:17 +0800

    example withorm

---
+++ examples/withorm.nim
@@ -0,0 +1,35 @@
+import norm/postgres
+import nimgeo
+
+include env
+
+db("localhost", username, password, database):
+  type
+    City = object
+      name: string
+      # position: string
+      position {.
+        dbType: "GEOMETRY(POINT)"
+        parseIt: parseWkbPoint(it)
+        formatIt: it.toWkb
+      .}: Point
+
+when isMainModule:
+  withDb:
+    createTables(force=true)
+
+    # var city = City(name: "beijing", position: "0101000000000000000000F03F0000000000000040")
+    var city = City(
+      name: "beijing",
+      position: newPoint(1.0, 2.0)
+    )
+    city.insert()
+    echo city.id
+    echo city.position.coord
+  
+  withDb:
+    let city = City.getOne(1)
+    echo repr city
+
+  withDb:
+    dropTables()
